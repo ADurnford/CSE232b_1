4d9e22468589430eda3fe4bfebf99ce864e9d265
Andrew <a714293@gmail.com>
2017-02-02 09:15:40 -0800

    Rename

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,6 +4,7 @@
 public class main {
     public static void main(String args[])
     {
-
+        String xmlFile = "C:\\CSE232B\\cse232b\\j_caesar.xml";
+        String domFile = "C:\\CSE232B\\cse232b\\play.dtd";
     }
 }
